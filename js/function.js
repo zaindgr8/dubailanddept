74d3556d0d52a82fecac87481f151db073039d30
zaindgr8 <66069515+zaindgr8@users.noreply.github.com>
2024-07-01 18:02:43 +0400

    602

---
+++ js/function.js
@@ -0,0 +1,18 @@
+/* ----------------------------------------------------------------------------------------
+* Author        : Awaiken
+* Template Name : App Launch - App Landing Page HTML5 Template
+* File          : Custom JS
+* Version       : 1.0
+* ---------------------------------------------------------------------------------------- */
+(function ($) {
+    "use strict";
+	
+	var $window = $(window); 
+	
+	/* Popup video */
+	$('.popup-video').magnificPopup({
+        type: 'iframe',
+        preloader: true,
+    });
+	
+})(jQuery);
